4036c94d665d4c1c4ae30b6858f2f8e3b2030a63
Antonio Magalhaes <antoniofmagalhaes@protonmail.com>
2021-01-21 20:30:35 -0400

    create users table migration

---
+++ src/database/migrations/20210121093741_create_users_table.ts
@@ -0,0 +1,16 @@
+import * as Knex from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable('users', table => {
+    table.increments('id')
+    table.string('name').notNullable()
+    table.string('email').notNullable().unique()
+    table.string('password').notNullable()
+    table.timestamp('created_at').defaultTo(new Date().toISOString())
+    table.timestamp('updated_at').defaultTo(new Date().toISOString())
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable('users')
+}
