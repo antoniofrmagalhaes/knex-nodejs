7c5a3c2c6a55f424f061121d5ad9c7015e902bf7
Antonio Magalhaes <antoniofmagalhaes@protonmail.com>
2021-01-21 20:32:33 -0400

    users routes set

--- src/core/routes/index.ts
+++ src/core/routes/index.ts
@@ -1,9 +1,12 @@
 import { Request, Response, Router } from 'express'
+import usersRoute from '../../modules/users/routes/users.routes'
 
 const routes = Router()
 
 routes.get('/', (_: Request, response: Response) => {
   return response.json({ message: 'REST API v0.0.1' })
 })
+
+routes.use(usersRoute)
 
 export default routes
