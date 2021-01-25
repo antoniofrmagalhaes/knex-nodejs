f3b3cbe587c6b58ecec79c1a59e740cdadf32f07
Antonio Magalhaes <antoniofmagalhaes@protonmail.com>
2021-01-25 13:38:14 -0400

    created timestampscolumns

--- src/database/migrations/20210121093741_create_users_table.ts
+++ src/database/migrations/20210121093741_create_users_table.ts
@@ -2,7 +2,7 @@ import * as Knex from 'knex'
 
 export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable('users', table => {
-    table.increments('id')
+    table.increments('id').primary()
     table.string('name').notNullable()
     table.string('email').notNullable().unique()
     table.string('password').notNullable()
