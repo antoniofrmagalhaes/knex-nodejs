4ac48e61774df5dad917c2e57f33fb1e467a18ce
Antonio Magalhaes <antoniofmagalhaes@protonmail.com>
2021-01-27 10:32:40 -0400

    filename name changed

--- src/modules/users/providers/hash/bcrypt/BCrypt.ts
+++ src/modules/users/providers/hash/bcrypt/BCrypt.ts
@@ -2,17 +2,14 @@ import { hash, compare } from 'bcryptjs'
 
 import HashProvider from '../models/HashProvider'
 
-class BcryptHashProvider implements HashProvider {
+class Bcrypt implements HashProvider {
   public async generateHash(payload: string): Promise<string> {
     return hash(payload, 8)
   }
 
   public async compareHash(payload: string, hashed: string): Promise<boolean> {
-    if (!hashed) {
-      return false
-    }
     return compare(payload, hashed)
   }
 }
 
-export default BcryptHashProvider
+export default Bcrypt
